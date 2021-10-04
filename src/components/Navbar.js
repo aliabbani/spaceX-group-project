ce7b551a2a720e48548e2fe1ca01a871db499906
Reshy <reshy@protonmail.com>
2021-10-04 11:58:59 +0200

    Add styling for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,21 +23,24 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="navBar">
-      <div className="left-nav">
-        <img src={logo} alt="logo" />
-        <h1>Space Travelers Hub</h1>
-      </div>
-      <ul>
-        {links.map((link) => (
-          <li className="right-nav-li" key={link.id}>
-            <NavLink to={link.path} className="left-nav-a" activeClassName="active-link" exact>
-              {link.text}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <nav className="navBar">
+        <div className="left-nav">
+          <img src={logo} alt="logo" className="logo-nav" />
+          <h1 className="main-title">Space Travelers Hub</h1>
+        </div>
+        <ul className="nav-elements">
+          {links.map((link) => (
+            <li className="right-nav-li" key={link.id}>
+              <NavLink to={link.path} className="left-nav-a" activeClassName="active-link" exact>
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <hr className="line-bot" />
+    </>
   );
 };
 
