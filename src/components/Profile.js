016c3e0e99864ec7a2c39d69e55dba8a76acc22c
Reshy <reshy@protonmail.com>
2021-10-05 12:47:19 +0200

    Add reservations on profile tab

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ProfileRocketsContainer from './ProfileRocketsContainer';
 
 const Profile = () => (
   <div>
-    <h1> hello from the profile page</h1>
+    <h3 className="mx-5 my-4"> Reserved rockets</h3>
+    <ProfileRocketsContainer />
   </div>
 );
 
